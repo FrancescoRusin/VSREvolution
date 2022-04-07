8aa7420372266602fb314637b32d52c35e1a5fb2
Francesco <rusinfrank98@gmail.com>
2022-04-07 12:24:09 +0100

    Started building the VSR breaker function.

--- src/main/java/it/units/erallab/BreakGrid.java
+++ src/main/java/it/units/erallab/BreakGrid.java
@@ -9,6 +9,7 @@ public class BreakGrid {
         Grid<Boolean> possibilities = Grid.create(grid.getW(), grid.getH());
         for (int w = 1; w < grid.getW() - 1; w++) {
             for (int h = 1; h < grid.getH() - 1; h++) {
+                possibilities.set(w, h, Boolean.FALSE);
                 if (!(Objects.isNull(grid.get(w, h))
                         && Objects.isNull(grid.get(w - 1, h))
                         && Objects.isNull(grid.get(w, h - 1))
@@ -17,12 +18,14 @@ public class BreakGrid {
                     possibilities.set(w, h, Boolean.TRUE);
                 }
             }
+            possibilities.set(w, 0, Boolean.FALSE);
             if (!(Objects.isNull(grid.get(w, 0))
                     && Objects.isNull(grid.get(w - 1, 0))
                     && Objects.isNull(grid.get(w + 1, 0))
                     && Objects.isNull(grid.get(w, 1)))) {
                 possibilities.set(w, 0, Boolean.TRUE);
             }
+            possibilities.set(w, grid.getH() - 1, Boolean.FALSE);
             if (!(Objects.isNull(grid.get(w, grid.getH() - 1))
                     && Objects.isNull(grid.get(w - 1, grid.getH() - 1))
                     && Objects.isNull(grid.get(w + 1, grid.getH() - 1))
@@ -31,12 +34,14 @@ public class BreakGrid {
             }
         }
         for (int h = 1; h < grid.getH() - 1; h++) {
+            possibilities.set(0, h, Boolean.FALSE);
             if (!(Objects.isNull(grid.get(0, h))
                     && Objects.isNull(grid.get(0, h - 1))
                     && Objects.isNull(grid.get(1, h))
                     && Objects.isNull(grid.get(0, h + 1)))) {
                 possibilities.set(0, h, Boolean.TRUE);
             }
+            possibilities.set(grid.getW() - 1, h, Boolean.FALSE);
             if (!(Objects.isNull(grid.get(grid.getW() - 1, h))
                     && Objects.isNull(grid.get(grid.getW() - 1, h - 1))
                     && Objects.isNull(grid.get(grid.getW() - 2, h))
@@ -44,35 +49,48 @@ public class BreakGrid {
                 possibilities.set(grid.getW(), h, Boolean.TRUE);
             }
         }
+        possibilities.set(0, 0, Boolean.FALSE);
         if (!(Objects.isNull(grid.get(0, 0))
                 && Objects.isNull(grid.get(1, 0))
                 && Objects.isNull(grid.get(0, 1)))) {
             possibilities.set(0, 0, Boolean.TRUE);
         }
-        if (!(Objects.isNull(grid.get(grid.getW(), grid.getH()))
-                && Objects.isNull(grid.get(grid.getW() - 1, grid.getH()))
-                && Objects.isNull(grid.get(grid.getW(), grid.getH() - 1)))) {
-            possibilities.set(grid.getW(), grid.getH(), Boolean.TRUE);
+        possibilities.set(grid.getW() - 1, grid.getH() - 1, Boolean.FALSE);
+        if (!(Objects.isNull(grid.get(grid.getW() - 1, grid.getH() - 1))
+                && Objects.isNull(grid.get(grid.getW() - 2, grid.getH() - 1))
+                && Objects.isNull(grid.get(grid.getW() - 1, grid.getH() - 2)))) {
+            possibilities.set(grid.getW() - 1, grid.getH() - 1, Boolean.TRUE);
         }
         return possibilities;
     }
 
     public static Set<Grid<Boolean>> crush(Grid<Boolean> baseline, int editDistance) {
         Grid<Boolean> possibilities=BreakGrid.getPossibilities(baseline);
-        List currentPossibilities=new ArrayList();
-        currentPossibilities.add(Grid.copy(possibilities));
-        List temporaryPossibilities=new ArrayList<Grid<Boolean>>();
+        Set<Grid<Boolean>>[] totalPossibilities=new Set[editDistance+1];
+        totalPossibilities[0]=Set.of(possibilities);
+        Set<Grid<Boolean>> temporaryPossibilities=new HashSet();
+        Grid<Boolean> placeholder;
         for(int counter=0;counter<editDistance;counter++) {
-            for(Grid<Boolean> entry : currentPossibilities) {
+            for(Grid<Boolean> entry : totalPossibilities[counter]) {
                     for(int i=0;i< baseline.getW();i++){
                         for(int j=0;j< baseline.getH();j++){
                             if(Objects.isNull(entry.get(i,j))){
                                 continue;
                             }
-
+                            placeholder=Grid.copy(entry);
+                            placeholder.set(i,j,!placeholder.get(i,j));
+                            temporaryPossibilities.add(Grid.copy(placeholder));
                         }
                     }
                 }
+            for(Grid<Boolean> poss : temporaryPossibilities){
+                for(int j=0;j<counter;j++){
+                    if(totalPossibilities[j].contains(poss)){
+                        temporaryPossibilities.remove(poss);
+                    }
+                }
+            }
+            totalPossibilities[counter+1].addAll(temporaryPossibilities);
         }
     }
 }
