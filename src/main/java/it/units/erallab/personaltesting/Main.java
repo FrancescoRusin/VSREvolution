3ac546c71b3fa1dc48c5f9bf0486337676929e5f
Francesco <rusinfrank98@gmail.com>
2022-04-08 17:36:14 +0100

    Tried testing on main.

--- src/main/java/it/units/erallab/personaltesting/Main.java
+++ src/main/java/it/units/erallab/personaltesting/Main.java
@@ -1,12 +1,22 @@
 package it.units.erallab.personaltesting;
 
 import it.units.erallab.hmsrobots.util.Grid;
+import it.units.erallab.locomotion.Starter;
 import it.units.erallab.personaltesting.BreakGrid;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-
+        List<String> arg=new ArrayList();
+        arg.add("episodeTime=30");
+        arg.add("shape=biped-6x4");
+        arg.add("sensorConfig=uniform-t-a-r");
+        arg.add("solver=numGA;nPop=10;nEval=10");
+        Starter TEST = new Starter(arg.toArray());
+        TEST.run();
     }
 }
