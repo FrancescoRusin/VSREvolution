c73b99d5646cf0e40f6f45c6d10a8cc99ff7bb69
Francesco <rusinfrank98@gmail.com>
2022-04-09 14:39:18 +0100

    Building tester.

--- src/main/java/it/units/erallab/personaltesting/Main.java
+++ src/main/java/it/units/erallab/personaltesting/Main.java
@@ -4,19 +4,19 @@ import it.units.erallab.hmsrobots.util.Grid;
 import it.units.erallab.locomotion.Starter;
 import it.units.erallab.personaltesting.BreakGrid;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> arg=new ArrayList();
-        arg.add("episodeTime=30");
+        List<String> arg=new ArrayList<String>();
+        //arg.add("episodeTime=30");
         arg.add("shape=biped-6x4");
-        arg.add("sensorConfig=uniform-t-a-r");
-        arg.add("solver=numGA;nPop=10;nEval=10");
-        Starter TEST = new Starter(arg.toArray());
+        arg.add("sensorConfig=uniform-t+r-0");
+        arg.add("mapper=brainHomoDist");
+        //arg.add("solver=numGA");
+        //arg.add("nPop=2");
+        //arg.add("nEval=2");
+        Starter TEST = new Starter(arg.toArray(new String[arg.size()]));
         TEST.run();
     }
 }
