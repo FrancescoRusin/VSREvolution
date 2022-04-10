b89a20dc5178d2c0043e55e645180829fc7d7667
Francesco <rusinfrank98@gmail.com>
2022-04-10 18:42:24 +0100

    Starting to see the light at the end of the tunnel; it may be hellfire though.

--- src/main/java/it/units/erallab/personaltesting/BreakDistMLP.java
+++ src/main/java/it/units/erallab/personaltesting/BreakDistMLP.java
@@ -1,6 +1,5 @@
 package it.units.erallab.personaltesting;
 
-import com.google.common.base.Stopwatch;
 import com.google.common.primitives.Doubles;
 import it.units.erallab.builder.function.MLP;
 import it.units.erallab.builder.robot.BrainHomoDistributed;
@@ -14,9 +13,9 @@ import it.units.erallab.hmsrobots.util.RobotUtils;
 import it.units.erallab.locomotion.Starter;
 import it.units.malelab.jgea.core.QualityBasedProblem;
 import it.units.malelab.jgea.core.TotalOrderQualityBasedProblem;
-import it.units.malelab.jgea.core.listener.Listener;
 import it.units.malelab.jgea.core.order.PartialComparator;
 import it.units.malelab.jgea.core.solver.IterativeSolver;
+import it.units.malelab.jgea.core.solver.SolverException;
 import it.units.malelab.jgea.core.solver.state.POSetPopulationState;
 
 import java.util.*;
@@ -26,26 +25,29 @@ import java.util.function.Function;
 
 import static it.units.erallab.hmsrobots.core.controllers.MultiLayerPerceptron.ActivationFunction.TANH;
 import static it.units.malelab.jgea.core.order.PartialComparator.PartialComparatorOutcome.*;
+import static java.util.function.Function.identity;
 
-public class BreakDistMLP implements QualityBasedProblem<double[][][],Outcome> {
+public class BreakDistMLP {
     private final Grid<Boolean> baseBody;
     private final String sensorsType;
     private final int signals;
     private final double episodeT;
     private final String terrainName;
-    private final int[] MLPstructure;
+    private final boolean diversity;
+    private final Function<Robot, Outcome> task;
 
-    public BreakDistMLP(Grid<Boolean> baseBody, String sensorsType, int signals, double episodeT, String terrainName, int[] MLPstructure) {
+    public BreakDistMLP(Grid<Boolean> baseBody, String sensorsType, int signals, double episodeT, String terrainName, boolean diversity) {
         this.baseBody = baseBody;
         this.sensorsType = sensorsType;
         this.signals = signals;
         this.episodeT = episodeT;
         this.terrainName = terrainName;
-        this.MLPstructure = MLPstructure;
+        this.diversity = diversity;
+        this.task=Starter.buildLocomotionTask(terrainName, episodeT, new Random(), false);
     }
 
     public BreakDistMLP(Grid<Boolean> baseBody, String sensorsType){
-        this(baseBody,sensorsType,1,10,"flat",new int[]{sensorsType.split(".\\+").length,5,2});
+        this(baseBody,sensorsType,1,10d,"flat", diversity);
     }
 
     public Function<double[][][], Outcome> qualityFunction() {
@@ -59,35 +61,26 @@ public class BreakDistMLP implements QualityBasedProblem<double[][][],Outcome> {
         return (x,y) -> (Doubles.compare(x.getVelocity(),y.getVelocity())>0 ? BEFORE : (Doubles.compare(x.getVelocity(),y.getVelocity())==0 ? SAME : AFTER));
     }
 
-    public Collection<double[][][]> solve(int nPop, int nEval) {
+    public Collection<List<Double>> solve(int nPop, int nEval) {
         Robot target = new Robot(
                 Controller.empty(),
                 RobotUtils.buildSensorizingFunction("uniform-" + sensorsType + "-0")
                         .apply(baseBody)
         );
         //build evolver
-        IterativeSolver<? extends POSetPopulationState<?, Robot, Outcome>,TotalOrderQualityBasedProblem<Robot, Outcome>, Robot> solver =
+        IterativeSolver<? extends POSetPopulationState<?, Robot, Outcome>, TotalOrderQualityBasedProblem<Robot, Outcome>, Robot> solver =
                 new DoublesStandard(0.75, 0.05, 3, 0.35)
-                .build(Map.ofEntries(Map.entry("nPop",String.valueOf(nPop)),Map.entry("nEval",String.valueOf(nEval))))
-                .build(new BrainHomoDistributed().build(Map.of("s",String.valueOf(signals)))
-                        .compose(new MLP().build(Map.ofEntries(Map.entry("just","placeholder")))),target);
+                        .build(Map.ofEntries(Map.entry("nPop", String.valueOf(nPop)), Map.entry("nEval", String.valueOf(nEval)), Map.entry("diversity", String.valueOf(diversity))))
+                        .build(new BrainHomoDistributed().build(Map.of("s", String.valueOf(signals)))
+                                .compose(new MLP().build(Map.ofEntries(Map.entry("just", "placeholder")))), target);
         //optimize TODO
-        Starter.Problem problem = new Starter.Problem(
-                RobotUtils.buildRobotTransformation(transformationName, random)
-                        .andThen(buildLocomotionTask(terrainName, episodeTime, random, cacheOutcome))
-                        .andThen(o -> o.subOutcome(
-                                episodeTransientTime,
-                                episodeTime
-                        )),
-                Comparator.comparing(fitnessFunction).reversed()
-        );
-        Collection<Robot> solutions = solver.solve(problem,new Random(), Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()), listener);
-        progressMonitor.notify((float) counter / nOfRuns, String.format(
-                "(%d/%d); Done: %d solutions in %4ds",
-                counter,
-                nOfRuns,
-                solutions.size(),
-                stopwatch.elapsed(TimeUnit.SECONDS)
-        ));
+        Starter.Problem problem = new Starter.Problem(task, Comparator.comparing(Outcome::getVelocity).reversed());
+        Collection<Robot> solutions = new ArrayList<>();
+        try {
+            solutions = solver.solve(problem, new Random(), Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()));
+        } catch (SolverException e) {
+            System.out.println(String.format("Couldn't solve due to %s", e));
+        }
+        Collection<List<Double>> weights = solutions.stream().map(r -> r.getController()).map(c -> )
     }
 }
