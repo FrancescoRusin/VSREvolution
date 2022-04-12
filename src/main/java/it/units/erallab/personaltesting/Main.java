9c957404b033038a779fb69463bd78d06f678ef6
Francesco <rusinfrank98@gmail.com>
2022-04-12 14:52:05 +0100

    All done and ready for execution.

--- src/main/java/it/units/erallab/personaltesting/Main.java
+++ src/main/java/it/units/erallab/personaltesting/Main.java
@@ -16,17 +16,17 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Grid<Boolean>[] bodies = new Grid[3];
-        bodies[0]=Grid.create(4, 3, (x, y) -> Math.abs(x-1.5)>1 || y>0);
-        bodies[1]=Grid.create(7, 2, (x, y) -> y==1 || x%2==0);
-        bodies[2]=Grid.create(5,2,(x,y) -> true);
+        bodies[0] = Grid.create(4, 3, (x, y) -> Math.abs(x - 1.5) > 1 || y > 0);
+        bodies[1] = Grid.create(7, 2, (x, y) -> y == 1 || x % 2 == 0);
+        bodies[2] = Grid.create(5, 2, (x, y) -> true);
         String[] names = new String[3];
-        names[0]="biped";
-        names[1]="comb";
-        names[2]="worm";
+        names[0] = "biped";
+        names[1] = "comb";
+        names[2] = "worm";
         String distanceRunsString;
         String temp;
         BufferedWriter writer;
-        for(int counter=0;counter<10;counter++) {
+        for (int counter = 0; counter < 10; counter++) {
             for (int a = 0; a < 3; a++) {
                 List<Double>[] distanceRuns =
                         new BreakDistMLP(bodies[a], "t+r+vx+vy", 1, 30, "hilly-1-10-0", false)
@@ -37,11 +37,11 @@ public class Main {
                     for (Double s : distanceRun) {
                         temp += s.toString() + ",";
                     }
-                    temp = temp.substring(0,temp.length()-1);
+                    temp = temp.substring(0, temp.length() - 1);
                     distanceRunsString += temp + ";\n";
                 }
                 try {
-                    writer = new BufferedWriter(new FileWriter(names[a] + "_" + (counter+1) + ".csv"));
+                    writer = new BufferedWriter(new FileWriter(names[a] + "_" + (counter + 1) + ".csv"));
                     writer.write(distanceRunsString);
                     writer.close();
                 } catch (IOException e) {
