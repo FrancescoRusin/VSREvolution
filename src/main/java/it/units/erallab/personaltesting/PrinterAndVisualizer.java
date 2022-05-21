031ea7467fd6846425d734939808a692d13ff341
Francesco <rusinfrank98@gmail.com>
2022-05-21 20:29:31 +0100

    Started visualizing postevolve.

--- src/main/java/it/units/erallab/personaltesting/PrinterAndVisualizer.java
+++ src/main/java/it/units/erallab/personaltesting/PrinterAndVisualizer.java
@@ -12,21 +12,19 @@ import java.util.List;
 public class PrinterAndVisualizer {
     public static void main(String[] args) {
         try {
-            String name = "comb";
-            int counter = 9;
-            List<Robot>[] robots =
-                    Analyzer.deserializeRobots("C:\\Users\\Francesco\\Desktop\\Università\\Tesi\\Risultati\\Big preevolve\\big_robots_"
-                            + name + "_" + counter + ".csv");
-            List<Double>[] results =
-                    Analyzer.importResults("C:\\Users\\Francesco\\Desktop\\Università\\Tesi\\Risultati\\Big preevolve\\"
-                            + name + "_big_preevolve_" + counter + ".csv");
-            List<Robot> best4gen = new ArrayList<>();
-            for (int i = 0; i < results.length; i++) {
-                best4gen.add(robots[i].get(results[i].indexOf(Collections.max(results[i]))));
+            String name = "worm";
+            int number = 1;
+            List<Robot>[] robots = Analyzer.deserializeRobots(
+                    "C:\\Users\\Francesco\\Desktop\\Università\\Tesi\\Risultati\\Big postevolve\\big_robots_" + name + "_" + number + ".csv");
+            List<Double>[] results = Analyzer.importResults(
+                    "C:\\Users\\Francesco\\Desktop\\Università\\Tesi\\Risultati\\Big postevolve\\" + name + "_big_postevolve_" + number + ".csv");
+            List<Robot> best4 = new ArrayList<>();
+            for (int i = 0; i < robots.length; i++) {
+                best4.add(robots[i].get(results[i].indexOf(Collections.max(results[i]))));
             }
-            GridOnlineViewer.run(new Locomotion(30, Locomotion.createTerrain("flat"), new Settings()), best4gen);
-        } catch(Exception e){
-            System.out.println("Something went wrong: "+e);
+            GridOnlineViewer.run(new Locomotion(30, Locomotion.createTerrain("flat"), new Settings()), best4);
+        } catch (Exception e) {
+            System.out.println("Something went wrong: " + e);
         }
     }
 }
