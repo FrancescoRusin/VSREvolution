0f3aa6abc333428813f2f1c78fa86499b838b2bf
Francesco <rusinfrank98@gmail.com>
2022-05-24 11:41:39 +0100

    Went on visualizing postevolve.

--- src/main/java/it/units/erallab/personaltesting/PrinterAndVisualizer.java
+++ src/main/java/it/units/erallab/personaltesting/PrinterAndVisualizer.java
@@ -13,7 +13,7 @@ public class PrinterAndVisualizer {
     public static void main(String[] args) {
         try {
             String name = "worm";
-            int number = 1;
+            int number = 2;
             List<Robot>[] robots = Analyzer.deserializeRobots(
                     "C:\\Users\\Francesco\\Desktop\\Università\\Tesi\\Risultati\\Big postevolve\\big_robots_" + name + "_" + number + ".csv");
             List<Double>[] results = Analyzer.importResults(
