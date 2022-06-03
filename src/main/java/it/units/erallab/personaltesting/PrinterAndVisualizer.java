b4c7e9c0e5bbedd6276db0aeb62e516c5dce8c01
Francesco <rusinfrank98@gmail.com>
2022-06-03 10:31:15 +0100

    Just checking results.

--- src/main/java/it/units/erallab/personaltesting/PrinterAndVisualizer.java
+++ src/main/java/it/units/erallab/personaltesting/PrinterAndVisualizer.java
@@ -13,7 +13,7 @@ public class PrinterAndVisualizer {
     public static void main(String[] args) {
         try {
             String name = "biped";
-            int number = 7;
+            int number = 9;
             List<Robot>[] robots = Analyzer.deserializeRobots(
                     "C:\\Users\\Francesco\\Desktop\\Università\\Tesi\\Risultati\\Big postevolve\\big_robots_" + name + "_" + number + ".csv");
             List<Double>[] results = Analyzer.importResults(
